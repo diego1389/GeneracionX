ca3dbd9925239eab4063a7178efee19491f7596a
Diego Guillén Hernández <dalguillen3089@gmail.com>
2018-09-01 09:19:00 -0600

    Play again

---
+++ GeneracionX-master/Assets/BehaviorScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BehaviorScript : MonoBehaviour {
+
+    // Use this for initialization
+   
+}
---
+++ GeneracionX-master/Assets/Scripts/Managers/ButtonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour {
+
+    public Button m_YourFirstButton;
+
+    void Start()
+    {
+        Button btn1 = m_YourFirstButton.GetComponent<Button>();
+
+        //Calls the TaskOnClick/TaskWithParameters method when you click the Button
+        btn1.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level01");
+    }
+}
